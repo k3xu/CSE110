5603f8b9a2d27725c4585ab7567243424273cc59
Kody Xu <k3xu@ucsd.edu>
2020-11-08 02:28:44 -0800

    message

--- src/edu/ucsd/cs110/tests/CelsiusTest.java
+++ src/edu/ucsd/cs110/tests/CelsiusTest.java
@@ -3,8 +3,6 @@ import edu.ucsd.cs110.temperature.Celsius;
 import edu.ucsd.cs110.temperature.Temperature;
 import junit.framework.TestCase;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CelsiusTest extends TestCase{
     private float delta = 0.001f;
